f55dbdf629b2bba19cc5a61194db00fb9bf77417
Nathan Myers <nathanemyers@gmail.com>
2016-09-21 14:01:48 -0500

    adding clean to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
+const del = require('del');
 
-gulp.task('js', function() {
-  return gulp.src('./src/js/*.js')
+gulp.task('js', () => {
+  return gulp.src('./src/js/d3_rankings.js')
+    .pipe(gulp.dest('./dist/index.js'));
+});
+
+gulp.task('html', () => {
+  return gulp.src('./src/index.html')
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   return gulp.src('./src/sass/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build', ['js', 'sass'], function() {
-  return;
-});
+gulp.task('clean', () => {
+  return del([ 'dist/*' ]);
+})
+
+gulp.task('build', ['clean', 'js', 'sass', 'html']);
