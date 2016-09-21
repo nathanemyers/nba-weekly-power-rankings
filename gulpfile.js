de482624443ed7829980b0a687f84fb6cebf537e
Nathan Myers <nathanemyers@gmail.com>
2016-09-21 15:50:52 -0500

    updating gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,26 @@ const del = require('del');
 
 gulp.task('js', () => {
   return gulp.src('./src/js/d3_rankings.js')
-    .pipe(gulp.dest('./dist/index.js'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('html', () => {
   return gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass', () => {
   return gulp.src('./src/sass/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('clean', () => {
-  return del([ 'dist/*' ]);
+  return del([ './dist/**' ]);
 })
 
-gulp.task('build', ['clean', 'js', 'sass', 'html']);
+gulp.task('build', ['js', 'sass', 'html']);
+
+gulp.task('watch', () => {
+  gulp.watch('./src/**', ['build']);
+})
